8bdfa846944ff3340d50739f44112c5a0fe5c4c4
pattamite <jittapat007@gmail.com>
2021-08-10 10:32:21 +0700

    Add assignment 1 of section 2

---
+++ Section2/basics-assignment-1/assignment1.js
@@ -0,0 +1,9 @@
+const startValue = 20;
+let result = 18 + startValue;
+
+result = result - 10;
+result = result * 2;
+result = result / 4;
+
+alert( `result :${result}` );
+alert( `user input :${startValue}` );
